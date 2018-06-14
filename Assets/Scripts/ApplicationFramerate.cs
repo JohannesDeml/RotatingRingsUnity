054e3f6a0dddb86e837edd0e07117558d4283172
Johannes Deml <send@johannesdeml.com>
2018-06-14 17:00:30 +0200

    Add project

---
+++ Assets/Scripts/ApplicationFramerate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationFramerate : MonoBehaviour {
+
+    [SerializeField]
+    private int targetFramerate = 60;
+
+    [SerializeField]
+    private bool applyOnStart = true;
+
+    [SerializeField]
+    private bool applyOnApplicationFocus = true;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        if(applyOnStart) 
+        {
+            ApplyTargetFramerate();
+        }
+	}
+
+    private void OnApplicationPause(bool pause)
+    {
+        if(!pause && applyOnApplicationFocus)
+        {
+            ApplyTargetFramerate();
+        }
+    }
+
+    private void ApplyTargetFramerate() 
+    {
+        Application.targetFrameRate = targetFramerate;
+    }
+}
